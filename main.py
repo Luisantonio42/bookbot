61411be7b3efdbff5c5b7f28246cd756131a49b2
Luis <luisgarmir@hotmail.com>
2025-09-09 18:11:16 -0600

    arguments improved

--- main.py
+++ main.py
@@ -1,25 +1,27 @@
 from stats import *
-
-f_file = "/Users/luisgarcia/Documents/Projects/coding_exercises/backend_dev/c4_bookbot/bookbot/books/frankenstein.txt"
-
+import sys
 
 def main():
-    print("============ BOOKBOT ============")
-    print(f"Analyzing book found at: {f_file}")
+    print(len(sys.argv))
+    if len(sys.argv) == 2:
+        print("============ BOOKBOT ============")
+        print(f"Analyzing book found at: {sys.argv[1]}")
 
-    words = count_words_from_text(f_file)
-    print("----------- Word Count ----------")
-    print(f"{words} words found in the document!")
+        words = count_words_from_text(sys.argv[1])
+        print("----------- Word Count ----------")
+        print(f"{words} words found in the document!")
 
-    ordered_chars = sort_chars_dictionary(count_characters_from_text(f_file))
-    print("--------- Character Count -------")
-    for char in ordered_chars:
-        x = char["char"]
-        y = char["num"]
-        if x.isalpha():
-            print(f"'{x}': {y}")
+        ordered_chars = sort_chars_dictionary(count_characters_from_text(sys.argv[1]))
+        print("--------- Character Count -------")
+        for char in ordered_chars:
+            x = char["char"]
+            y = char["num"]
+            if x.isalpha():
+                print(f"'{x}': {y}")
 
-    print("============= END ===============")
+        print("============= END ===============")
+    else:
+        print("Usage: python3 main.py <path_to_book>")
 
 
 main()
